60c7518022a8bffefed272ae537ed60ed00c856d
joe <>
2022-07-09 14:35:10 +0800

    完成範例撰寫

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,148 @@
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 import { Component } from '@angular/core';
+import { NewEmployeeInfoForm } from './models/form/new-employee-info-form.model';
+import { EmergencyContactForm } from './models/form/emergency-contact-form.model';
+import { ToForm } from './models/to-form.model';
+import { NewEmployeeInfo } from './models/data/new-employee-info.model';
+import { EmergencyContact } from './models/data/emergency-contact.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-strict-typed-forms-demo';
+
+  // <--- 沒有型別的表單(自動推導)
+
+  newEmployeeInfoForm = new FormGroup({
+    chineseName: new FormControl(''),
+    englishName: new FormControl(''),
+    contactNumber: new FormControl(''),
+    residentAddress: new FormControl(''),
+    mailingAddress: new FormControl(''),
+    sameResidentAddress: new FormControl(false),
+    emergencyContacts: new FormArray([this.createEmergencyContactForm()]),
+  });
+
+  get newEmployeeInfoFormControls() {
+    return this.newEmployeeInfoForm.controls;
+  }
+
+  // 沒有型別的表單(自動推導) --->
+
+
+  // <--- 有型別的表單(一)
+
+  // newEmployeeInfoForm: FormGroup<NewEmployeeInfoForm> = new FormGroup({
+  //   chineseName: new FormControl(''),
+  //   englishName: new FormControl(''),
+  //   contactNumber: new FormControl(''),
+  //   residentAddress: new FormControl(''),
+  //   mailingAddress: new FormControl(''),
+  //   sameResidentAddress: new FormControl(false),
+  //   emergencyContacts: new FormArray([this.createEmergencyContactForm()]),
+  // });
+
+  // get newEmployeeInfoFormControls(): NewEmployeeInfoForm {
+  //   return this.newEmployeeInfoForm.controls;
+  // }
+
+  // 有型別的表單(一) --->
+
+
+  // <--- 有型別的表單(二)
+
+  // newEmployeeInfoForm: FormGroup<ToForm<NewEmployeeInfo>> = new FormGroup({
+  //   chineseName: new FormControl(''),
+  //   englishName: new FormControl(''),
+  //   contactNumber: new FormControl(''),
+  //   residentAddress: new FormControl(''),
+  //   mailingAddress: new FormControl(''),
+  //   sameResidentAddress: new FormControl(false),
+  //   emergencyContacts: new FormArray([this.createEmergencyContactForm()]),
+  // });
+
+  // get newEmployeeInfoFormControls(): ToForm<NewEmployeeInfo> {
+  //   return this.newEmployeeInfoForm.controls;
+  // }
+
+  // 有型別的表單(二) --->
+
+  ngOnInit(): void {
+
+    this.newEmployeeInfoFormControls.sameResidentAddress.valueChanges.subscribe(
+      (val) => {
+        if (val) {
+          this.newEmployeeInfoFormControls.mailingAddress.setValue(
+            this.newEmployeeInfoFormControls.residentAddress.value
+          );
+          this.newEmployeeInfoFormControls.mailingAddress.disable();
+        } else {
+          this.newEmployeeInfoFormControls.mailingAddress.setValue('');
+          this.newEmployeeInfoFormControls.mailingAddress.enable();
+        }
+      }
+    );
+
+    // console.log(this.newEmployeeInfoFormControls.note?.value);
+    // console.log(this.newEmployeeInfoFormControls.blaBlaBla?.value);
+    // console.log(this.newEmployeeInfoFormControls.emergencyContacts.value[0].contactNumber);
+
+  }
+
+  // <--- 沒有型別 (自動推導)
+
+  createEmergencyContactForm(){
+    return new FormGroup({
+      name: new FormControl(''),
+      relation: new FormControl(''),
+      contactNumber: new FormControl(''),
+    });
+  }
+
+  // 沒有型別 (自動推導) --->
+
+  // <--- 有型別的表單(一)
+
+  // createEmergencyContactForm(): FormGroup<EmergencyContactForm> {
+  //   return new FormGroup({
+  //     name: new FormControl(''),
+  //     relation: new FormControl(''),
+  //     contactNumber: new FormControl(''),
+  //   });
+  // }
+
+  // 有型別的表單(一) --->
+
+
+  // <--- 有型別的表單(二)
+
+  // createEmergencyContactForm(): FormGroup<ToForm<EmergencyContact>> {
+  //   return new FormGroup({
+  //     name: new FormControl(''),
+  //     relation: new FormControl(''),
+  //     contactNumber: new FormControl(''),
+  //   });
+  // }
+
+  // 有型別的表單(二) --->
+
+  addEmergencyContact(): void {
+    this.newEmployeeInfoFormControls.emergencyContacts.push(
+      this.createEmergencyContactForm()
+    );
+  }
+
+  removeEmergencyContact(index: number): void {
+    this.newEmployeeInfoFormControls.emergencyContacts.removeAt(index);
+  }
+
+  save(): void {
+    console.log(
+      'newEmployeeInfoForm.getRawValue',
+      this.newEmployeeInfoForm.getRawValue()
+    );
+  }
+
 }

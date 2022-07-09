1638d6c8f89ad888d28d8c155b462793d3cc2680
joe <>
2022-07-09 17:37:56 +0800

    調整範例

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
 import { Component } from '@angular/core';
+
 import { NewEmployeeInfoForm } from './models/form/new-employee-info-form.model';
 import { EmergencyContactForm } from './models/form/emergency-contact-form.model';
+
 import { ToForm } from './models/to-form.model';
+
 import { NewEmployeeInfo } from './models/data/new-employee-info.model';
 import { EmergencyContact } from './models/data/emergency-contact.model';
+
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +17,6 @@ import { EmergencyContact } from './models/data/emergency-contact.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   // <--- 沒有型別的表單(自動推導)
 
   newEmployeeInfoForm = new FormGroup({
@@ -31,7 +35,6 @@ export class AppComponent {
 
   // 沒有型別的表單(自動推導) --->
 
-
   // <--- 有型別的表單(一)
 
   // newEmployeeInfoForm: FormGroup<NewEmployeeInfoForm> = new FormGroup({
@@ -49,7 +52,6 @@ export class AppComponent {
   // }
 
   // 有型別的表單(一) --->
-
 
   // <--- 有型別的表單(二)
 
@@ -73,6 +75,7 @@ export class AppComponent {
 
     this.newEmployeeInfoFormControls.sameResidentAddress.valueChanges.subscribe(
       (val) => {
+
         if (val) {
           this.newEmployeeInfoFormControls.mailingAddress.setValue(
             this.newEmployeeInfoFormControls.residentAddress.value
@@ -82,18 +85,28 @@ export class AppComponent {
           this.newEmployeeInfoFormControls.mailingAddress.setValue('');
           this.newEmployeeInfoFormControls.mailingAddress.enable();
         }
+
       }
     );
+
+    this.newEmployeeInfoFormControls.residentAddress.valueChanges
+      .pipe(
+        filter(
+          (val) => !!this.newEmployeeInfoFormControls.sameResidentAddress.value
+        )
+      )
+      .subscribe((address) => {
+        this.newEmployeeInfoFormControls.mailingAddress.setValue(address);
+      });
 
     // console.log(this.newEmployeeInfoFormControls.note?.value);
     // console.log(this.newEmployeeInfoFormControls.blaBlaBla?.value);
     // console.log(this.newEmployeeInfoFormControls.emergencyContacts.value[0].contactNumber);
-
   }
 
   // <--- 沒有型別 (自動推導)
 
-  createEmergencyContactForm(){
+  createEmergencyContactForm() {
     return new FormGroup({
       name: new FormControl(''),
       relation: new FormControl(''),
@@ -114,7 +127,6 @@ export class AppComponent {
   // }
 
   // 有型別的表單(一) --->
-
 
   // <--- 有型別的表單(二)
 
@@ -144,5 +156,4 @@ export class AppComponent {
       this.newEmployeeInfoForm.getRawValue()
     );
   }
-
 }
